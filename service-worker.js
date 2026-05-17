cc7f0ee53b7e4195d37cf214455b19f7f323450d
Marzenego <filippo.cortella5@gmail.com>
2026-05-17 15:58:34 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-self.addEventListener('install', e => {
-  self.skipWaiting();
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    fetch(event.request)
+  );
 });
-
-self.addEventListener('fetch', e => {});
